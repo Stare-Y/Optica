ab7e5a2133c249dbb4300c37c9d241d155449243
Kevin Becerra <kbecerra774@gmail.com>
2024-09-02 21:02:05 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Lote.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Lote
+    {
+        public int Id { get; set; }
+        public int Referencia { get; set; }
+        public DateTime FechaEntrada { get; set; }
+        //necesitamos una tabla intermedia, para saber cuantos micas hay en un lote y de que tipos y graduaciones
+
+
+    }
+}
---
+++ Domain/Entities/Pedido.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public bool Tipo { get; set; } //alta 1 o baja 0
+        public DateTime FechaSalida { get; set; }
+    }
+}
