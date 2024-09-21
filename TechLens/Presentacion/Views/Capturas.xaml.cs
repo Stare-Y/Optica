be161aa5f89787389f9372b3e0091c2ff6daf76a
Erick Garcia <erick-noel_123@outlook.com>
2024-09-21 17:38:33 -0600

    Desarrollo de la interfaz de Captura de datos finalizada - en espera a ligar con backend

--- TechLens/Presentacion/Views/Capturas.xaml.cs
+++ TechLens/Presentacion/Views/Capturas.xaml.cs
@@ -6,4 +6,21 @@ public partial class Capturas : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void BtnCancelar_Clicked(object sender, EventArgs e)
+    {
+		BtnCancelar.Opacity = 0;
+		await BtnCancelar.FadeTo(1, 200);
+
+		await Shell.Current.GoToAsync("..");
+    }
+
+    private async void BtnGuardar_Clicked(object sender, EventArgs e)
+    {
+		BtnGuardar.Opacity = 0;
+        await BtnGuardar.FadeTo(1, 200);
+
+		await DisplayAlert("Guardado", "Se ha guardado la captura de datos", "Aceptar");
+		await Shell.Current.GoToAsync("..");
+    }
 }
