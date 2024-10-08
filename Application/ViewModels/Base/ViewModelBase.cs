4d4ba7f41e52b77ad826484185557cd31601b07f
pointasty <kbecerra774@gmail.com>
2024-10-08 01:27:33 -0600

    Quite todos los warnings, ya no mas ansiedad por esas mierdas gnte, sisas

--- Application/ViewModels/Base/ViewModelBase.cs
+++ Application/ViewModels/Base/ViewModelBase.cs
@@ -4,7 +4,7 @@ namespace Application.ViewModels.Base
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
         {
--- TechLens/Presentacion/Views/Captura/GraduacionMica.xaml.cs
+++ TechLens/Presentacion/Views/Captura/GraduacionMica.xaml.cs
@@ -27,58 +27,59 @@ public partial class GraduacionMica : ContentPage
     private void entryEsfera_TextChanged(object sender, TextChangedEventArgs e)
     {
        var entry = sender as Entry;
-       var text = entry.Text;
-
-        // Permite números, punto y guión solo en la primera posición.
-        if (!string.IsNullOrEmpty(text))
+       if(entry != null)
         {
-            text = new string(text.Where((c, i) => char.IsDigit(c) || c == '.' || (i == 0 && c == '-')).ToArray());
-        }
+            var text = entry.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                text = new string(text.Where((c, i) => char.IsDigit(c) || c == '.' || (i == 0 && c == '-')).ToArray());
+            }
 
-        // Asegurarse de que el guión solo esté en la primera posición.
-        if (text.Contains("-") && text.IndexOf('-') > 0)
-        {
-            text = text.Replace("-", "");
-        }
+            if (text.Contains("-") && text.IndexOf('-') > 0)
+            {
+                text = text.Replace("-", "");
+            }
 
-        if (text.Length > 7) // 6 caracteres más el guión opcional.
-        {
-            text = text.Substring(0, 7);
-        }
+            if (text.Length > 7)
+            {
+                text = text.Substring(0, 7);
+            }
 
-        if (entry.Text != text)
-        {
-            entry.Text = text;
+            if (entry.Text != text)
+            {
+                entry.Text = text;
+            }
         }
-
     }
 
     private void entryCilindro_TextChanged(object sender, TextChangedEventArgs e)
     {
         var entry = sender as Entry;
-        var text = entry.Text;
-
-        // Permite números, punto y guión solo en la primera posición.
-        if (!string.IsNullOrEmpty(text))
+        if (entry != null)
         {
-            text = new string(text.Where((c, i) => char.IsDigit(c) || c == '.' || (i == 0 && c == '-')).ToArray());
-        }
+            var text = entry.Text;
 
-        // Asegurarse de que el guión solo esté en la primera posición.
-        if (text.Contains("-") && text.IndexOf('-') > 0)
-        {
-            text = text.Replace("-", "");
-        }
+            // Permite números, punto y guión solo en la primera posición.
+            if (!string.IsNullOrEmpty(text))
+            {
+                text = new string(text.Where((c, i) => char.IsDigit(c) || c == '.' || (i == 0 && c == '-')).ToArray());
+            }
 
-        if (text.Length > 7) // 6 caracteres más el guión opcional.
-        {
-            text = text.Substring(0, 7);
-        }
+            // Asegurarse de que el guión solo esté en la primera posición.
+            if (text.Contains("-") && text.IndexOf('-') > 0)
+            {
+                text = text.Replace("-", "");
+            }
 
-        if (entry.Text != text)
-        {
-            entry.Text = text;
-        }
+            if (text.Length > 7) // 6 caracteres más el guión opcional.
+            {
+                text = text.Substring(0, 7);
+            }
 
+            if (entry.Text != text)
+            {
+                entry.Text = text;
+            }
+        }
     }
 }
