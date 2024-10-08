b4517eb4327c85427db7ea674999f5d0179a5966
pointasty <kbecerra774@gmail.com>
2024-10-08 01:20:15 -0600

    Removi las warnings de la implementacion, y no se q mas xd

--- Application/ViewModels/ViewModelCapturas.cs
+++ Application/ViewModels/ViewModelCapturas.cs
@@ -8,7 +8,7 @@ namespace Application.ViewModels
     {
         private Lote _lote = new Lote();
 
-        private readonly ILoteRepo _loteRepo;
+        private readonly ILoteRepo? _loteRepo;
 
         public ViewModelCapturas(ILoteRepo loteRepo)
         {
@@ -27,17 +27,17 @@ namespace Application.ViewModels
             }
         }
 
-        public void GoToCapturarMicas()
-        {
-            _loteRepo.ValidarLote(_lote);
-            //viermodel de captura de micas, recibe la instancia de lote capturada
-            //await Shell.Current.GoToAsync("CapturarMicas");
-        }
-
         public async Task Initialize()
         {
-            _lote.Id = await _loteRepo.GetSiguienteId();
-            OnPropertyChanged(nameof(Lote));
+            if (_loteRepo is not null)
+            {
+                _lote.Id = await _loteRepo.GetSiguienteId();
+                OnPropertyChanged(nameof(Lote));
+            }
+            else
+            {
+                throw new Exception("No se ha inyectado el repositorio de lotes");
+            }
         }
     }
 }
