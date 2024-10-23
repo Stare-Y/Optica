474a41c6fae637a6705965c2bb411a2d328b0838
pointasty <kbecerra774@gmail.com>
2024-10-22 22:30:02 -0600

    reporte view ready, now missing sonme frontend animation fixes

--- TechLens/Presentacion/Views/Reportes.xaml.cs
+++ TechLens/Presentacion/Views/Reportes.xaml.cs
@@ -1,29 +1,33 @@
 using Application.ViewModels;
-using Domain.Entities;
-using TechLens.Presentacion.Events;
 
 namespace TechLens.Presentacion.Views;
 
 public partial class Reportes : ContentPage
 {
-	public Reportes()
+    private readonly ViewModelReportes _viewModel = null!;
+	public Reportes(ViewModelReportes viewModel)
 	{
 		InitializeComponent();
+        _viewModel = viewModel;
+        this.BindingContext = _viewModel;
 	}
 
-    private void CollectionViewMicas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    public Reportes() : this(MauiProgram.ServiceProvider.GetRequiredService<ViewModelReportes>())
     {
+    }
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.FechaInicio = DateTime.Now;
+        _viewModel.FechaFin = DateTime.Now;
     }
 
     private async void BtnRegresar_Clicked(object sender, EventArgs e)
     {
         BtnRegresar.Opacity = 0;
         await BtnRegresar.FadeTo(1, 200);
-
         await Shell.Current.GoToAsync("..");
-
-
     }
 
     private async void BtnImprimir_Clicked(object sender, EventArgs e)
@@ -31,5 +35,37 @@ public partial class Reportes : ContentPage
         BtnImprimir.Opacity = 0;
         await BtnImprimir.FadeTo(1, 200);
 
+        //Generar reporte
+        try
+        {
+            await _viewModel.ExportarReporteObtenido();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "Aceptar");
+        }
+    }
+
+    private void DatePickerFechaFin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        _viewModel.FechaFin = e.NewDate;
+    }
+
+    private void DatePickerFechaInicio_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        _viewModel.FechaFin = e.NewDate;
+    }
+
+    private async void BtnGenerarReporte_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await _viewModel.GetReportePedidos();
+            BtnImprimir.IsEnabled = true;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "Aceptar");
+        }
     }
 }
