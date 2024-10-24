47bc973b9374e4500db3fb408e14625273f857f5
EDUARDO FRANCISCO DIAZ CORONA <francisco.diaz7244@alumno.udg.mx>
2024-10-24 16:11:04 -0600

    excel

--- Application/ViewModels/ViewModelReportes.cs
+++ Application/ViewModels/ViewModelReportes.cs
@@ -2,6 +2,7 @@
 using Domain.Interfaces;
 using Domain.Interfaces.Services.Reportes.Entities;
 using System.Collections.ObjectModel;
+using ClosedXML.Excel;
 
 
 
@@ -63,7 +64,52 @@ namespace Application.ViewModels
         public void GenerarReporteExcel()
         {
             var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ReportesTechLens");
+
+            if (!Directory.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta);
+            }
+
+            var nombreArchivo = "ReportesTechLens.xlsx";
+            var path = Path.Combine(ruta, nombreArchivo);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Reporte");
+
+                worksheet.Cell(1, 1).Value = "ID Pedido";
+                worksheet.Cell(1, 2).Value = "Usuario";
+                worksheet.Cell(1, 3).Value = "Fabricante";
+                worksheet.Cell(1, 4).Value = "Tratamiento";
+                worksheet.Cell(1, 5).Value = "Propósito";
+                worksheet.Cell(1, 6).Value = "Fecha Salida";
+                worksheet.Cell(1, 7).Value = "Razón Social";
+                worksheet.Cell(1, 8).Value = "Graduación Esférica";
+                worksheet.Cell(1, 9).Value = "Graduación Cilíndrica";
+                worksheet.Cell(1, 10).Value = "Cantidad";
+                worksheet.Cell(1, 11).Value = "Precio";
+
+                for (int i = 0; i < ReportePedidos.Count; i++)
+                {
+                    var pedido = ReportePedidos[i];
+                    worksheet.Cell(i + 2, 1).Value = pedido.IdPedido;
+                    worksheet.Cell(i + 2, 2).Value = pedido.Usuario;
+                    worksheet.Cell(i + 2, 3).Value = pedido.Fabricante;
+                    worksheet.Cell(i + 2, 4).Value = pedido.Tratamiento;
+                    worksheet.Cell(i + 2, 5).Value = pedido.Proposito;
+                    worksheet.Cell(i + 2, 6).Value = pedido.FechaSalida.ToString("dd-MM-yyyy");
+                    worksheet.Cell(i + 2, 7).Value = pedido.RazonSocial;
+                    worksheet.Cell(i + 2, 8).Value = pedido.GraduacionEsferica;
+                    worksheet.Cell(i + 2, 9).Value = pedido.GraduacionCilindrica;
+                    worksheet.Cell(i + 2, 10).Value = pedido.Cantidad;
+                    worksheet.Cell(i + 2, 11).Value = pedido.Precio;
+                }
+                workbook.SaveAs(path);
+            }
+
+            Console.WriteLine($"Reporte generado exitosamente en: {path}");
         }
+
     }
 }
 
